c452fcbdf0ee8ada08c745c6514f56c054372c18
CodeFromRahul <rahulvish194002@gmail.com>
2024-03-04 23:53:36 +0530

    backend for liveShare

--- deta/index.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const { Deta } = require("deta");
-const deta = Deta();
-const base = deta.Base("pulp");
-const drive = deta.Drive("pulp");
-
-module.exports = { base, drive };
